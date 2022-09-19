6b96e0b8493247658ce103d76b93e0c4ac870f16
celso <celso.junior@casavaticano.com.br>
2022-09-19 16:39:10 -0300

    first commit

---
+++ jw2client.py
@@ -0,0 +1,74 @@
+import requests
+import json
+
+class File:
+    def __init__(self):
+        self.node_id  = 0
+        self.tenant_id = 0
+        self.folder_parent_id= 0
+        self.name = ''
+        self.type = ''
+        self.mime = ''
+        self.extension = ''
+        self.filesize = 0
+        self.content_hash = ''
+        self.users_id = 0
+        self.created_at = ''
+        self.updated_at = ''
+        self.google_document_id = ''
+
+class Jamworks:
+    def __init__(self,core_url,content_url):
+        self.core_url =  core_url
+        self.content_url = content_url
+        self.token = ''
+    
+    def auth(self,user,password):
+        auth_url = self.core_url+"/auth/login"
+        params = {'username':user,'password':password}
+        response = requests.post(url=auth_url,data=params)
+        ret = response.json()
+        self.token = ret['token']
+    
+    def getContentsFileInfo(self,node_id):
+        info_url = self.content_url+"/entry/"+str(node_id)
+        headers = {"token":self.token}
+
+        node_data_req = requests.get(url = info_url, headers = headers)
+
+        node_data = node_data_req.json()
+        print(node_data)
+        file = File()
+        file.node_id  = node_data['node_id']
+        file.tenant_id = node_data['tenant_id']
+        file.folder_parent_id = node_data['folder_parent_id']
+        file.name = node_data['name']
+        file.type = node_data['type']
+        file.mime = node_data['mime']
+        file.extension = node_data['extension']
+        file.filesize = node_data['filesize']
+        file.content_hash = node_data['content_hash']
+        file.users_id = node_data['users_id']
+        file.created_at = node_data['created_at']
+        file.updated_at = node_data['updated_at']
+        file.google_document_id = node_data['google_document_id']
+
+        return file
+
+    def contentsDownloadFile(self,node_id,filename):
+        download_url = self.content_url+"/actions/download/"+str(node_id)
+        headers = {"token":self.token}
+        with requests.get(url = download_url, headers = headers , stream = True) as r:
+            with open(filename,"wb") as f:
+                for chunk in r.iter_content(chunk_size = 16 * 1024):
+                    f.write(chunk)
+
+
+    def contentsUploadFile(self,tenant_id,parent_nodeid,filename):
+        upload_url = self.content_url+"/file"
+        headers = {'token': self.token}
+        data = {"tenant_id":tenant_id,"folder_parent_id":parent_nodeid}
+        files = { "file":open(filename,"rb")}
+        response = requests.post(url = upload_url, headers=headers, files=files, data=data)
+        r = response.json()
+        return self.getContentsFileInfo(r['node_id'])
